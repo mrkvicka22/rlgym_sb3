47a68031493646dbae6a29ef54ac1fa0e9522cf0
Matej <matej.novosad.mn@gmail.com>
2021-12-27 23:04:56 +0100

    fixed keepaway reward and implemented proximity based random state setter

--- custom_rewards/keepaway_rewards.py
+++ custom_rewards/keepaway_rewards.py
@@ -1,7 +1,9 @@
+import rlgym_compat
 from rlgym.utils.gamestates import GameState, PlayerData
 from rlgym.utils.reward_functions import RewardFunction
 from rlgym.utils.reward_functions.common_rewards.player_ball_rewards import VelocityPlayerToBallReward
 from numpy import ndarray
+from rlgym.utils.common_values import BLUE_TEAM, ORANGE_TEAM
 
 
 class KeepAwayReward(RewardFunction):
@@ -11,6 +13,7 @@ class KeepAwayReward(RewardFunction):
 
     def __init__(self):
         super().__init__()
+        self.player_ids = {BLUE_TEAM:{1,2,3,4},ORANGE_TEAM:{5,6,7,8}}
 
     def reset(self, initial_state: GameState):
         pass
@@ -19,5 +22,7 @@ class KeepAwayReward(RewardFunction):
         # check for touch occuring
         if state.last_touch == -1:
             return 0
-        if player.team_num == state.players[state.last_touch].team_num:
+        if state.last_touch in self.player_ids[player.team_num]:
             return 1
+        else:
+            return 0
--- training_keepaway_bot.py
+++ training_keepaway_bot.py
@@ -7,20 +7,28 @@ from stable_baselines3.common.vec_env import VecMonitor, VecNormalize, VecCheckN
 from stable_baselines3.ppo import MlpPolicy
 
 from rlgym.utils.obs_builders import AdvancedObs
-from rlgym.utils.reward_functions.common_rewards import VelocityPlayerToBallReward
+from rlgym.utils.reward_functions.common_rewards import VelocityBallToGoalReward, EventReward, VelocityPlayerToBallReward
+from rlgym.utils.reward_functions.combined_reward import CombinedReward
+from custom_rewards.keepaway_rewards import KeepAwayReward
 from rlgym.utils.state_setters import DefaultState
 from rlgym.utils.terminal_conditions.common_conditions import TimeoutCondition, GoalScoredCondition
 from rlgym_tools.sb3_utils import SB3MultipleInstanceEnv
+from rlgym_tools.extra_action_parsers.kbm_act import KBMAction
+from custom_state_setters.proximity_random_setter import ProximityRandomState
 
 if __name__ == '__main__':  # Required for multiprocessing
-    frame_skip = 8          # Number of ticks to repeat an action
-    half_life_seconds = 5   # Easier to conceptualize, after this many seconds the reward discount is 0.5
+    frame_skip = 8  # Number of ticks to repeat an action
+    half_life_seconds = 5  # Easier to conceptualize, after this many seconds the reward discount is 0.5
 
     fps = 120 / frame_skip
     gamma = np.exp(np.log(0.5) / (fps * half_life_seconds))  # Quick mafs
     print(f"fps={fps}, gamma={gamma})")
 
-    reward_func = # TODO: add some shit. 
+    reward_func = CombinedReward(
+        (VelocityBallToGoalReward(), EventReward(goal=10, concede=-10), KeepAwayReward(), VelocityPlayerToBallReward()),
+        (0.05, 1, 0.05, 0.01))
+
+
     def get_match():  # Need to use a function so that each instance can call it and produce their own objects
         return Match(
             team_size=3,  # 3v3 to get as many agents going as possible, will make results more noisy
@@ -29,29 +37,30 @@ if __name__ == '__main__':  # Required for multiprocessing
             self_play=True,
             terminal_conditions=[TimeoutCondition(round(fps * 30)), GoalScoredCondition()],  # Some basic terminals
             obs_builder=AdvancedObs(),  # Not that advanced, good default
-            state_setter=DefaultState(),  # Resets to kickoff position
-            action_parser=DiscreteAction()  # Discrete > Continuous don't @ me
+            state_setter=ProximityRandomState(),  # Resets to kickoff position
+            action_parser=KBMAction()  # Discrete > Continuous don't @ me
         )
 
-    env = SB3MultipleInstanceEnv(get_match, 2)            # Start 2 instances, waiting 60 seconds between each
-    env = VecCheckNan(env)                                # Optional
-    env = VecMonitor(env)                                 # Recommended, logs mean reward and ep_len to Tensorboard
+
+    env = SB3MultipleInstanceEnv(get_match, 2)  # Start 2 instances, waiting 60 seconds between each
+    env = VecCheckNan(env)  # Optional
+    env = VecMonitor(env)  # Recommended, logs mean reward and ep_len to Tensorboard
     env = VecNormalize(env, norm_obs=False, gamma=gamma)  # Highly recommended, normalizes rewards
 
     # Hyperparameters presumably better than default; inspired by original PPO paper
     model = PPO(
         MlpPolicy,
         env,
-        n_epochs=32,                 # PPO calls for multiple epochs
-        learning_rate=3e-5,          # Around this is fairly common for PPO
-        ent_coef=0.01,               # From PPO Atari
-        vf_coef=1.,                  # From PPO Atari
-        gamma=gamma,                 # Gamma as calculated using half-life
-        verbose=3,                   # Print out all the info as we're going
-        batch_size=4096*4,             # Batch size as high as possible within reason
-        n_steps=4096*4,                # Number of steps to perform before optimizing network
+        n_epochs=32,  # PPO calls for multiple epochs
+        learning_rate=3e-4,  # Around this is fairly common for PPO
+        ent_coef=0.01,  # From PPO Atari
+        vf_coef=1.,  # From PPO Atari
+        gamma=gamma,  # Gamma as calculated using half-life
+        verbose=3,  # Print out all the info as we're going
+        batch_size=4096 * 4,  # Batch size as high as possible within reason
+        n_steps=4096 * 4,  # Number of steps to perform before optimizing network
         tensorboard_log="out/logs",  # `tensorboard --logdir out/logs` in terminal to see graphs
-        device="auto",                # Uses GPU if available
+        device="auto",  # Uses GPU if available
         policy_kwargs={}
     )
 
