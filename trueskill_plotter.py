f79919351269d455983585e0567e31bc87579a19
Matej <matej.novosad.mn@gmail.com>
2021-12-30 00:56:30 +0100

    generalized for order function instead of hardcoding

--- trueskill_plotter.py
+++ trueskill_plotter.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 with open("policy_ratings", "rb") as f:
     ratings_database = pickle.load(f)
 
-plt.plot([int(ratings_database['opponents'][i]["name"].split("_")[1]) for i in range(len(ratings_database['opponents']))],[ratings_database['opponents'][i]["rating"].mu for i in range(len(ratings_database['opponents']))] )
+order_function = lambda x: int(x.split('_')[2])
+plt.plot([order_function(ratings_database['opponents'][i]["name"]) for i in range(len(ratings_database['opponents']))],[ratings_database['opponents'][i]["rating"].mu for i in range(len(ratings_database['opponents']))] )
 plt.ylabel('mu')
 plt.show()
