41fc3282b361d34ad24cf133984914088d6aefe3
Matej <matej.novosad.mn@gmail.com>
2022-01-05 15:49:54 +0100

    added docstrings and type hints

--- ssl_replays_for_setter/construct_batched_gamestates.py
+++ ssl_replays_for_setter/construct_batched_gamestates.py
@@ -14,6 +14,14 @@ def absolute_file_paths(directory: str):
 
 
 def convert_replays(paths_to_each_replay: List[str], frame_skip: int = 150, verbose: int = 0) -> np.ndarray:
+    """
+    Converts replays into
+
+    :param paths_to_each_replay: List of paths to each replay to be converted
+    :param frame_skip: Every frame_skip frame from the replay will be converted
+    :param verbose: When set to more than 0, it prints debug info
+    :return: Numpy array of frames
+    """
     states = []
     for replay in paths_to_each_replay:
         make_state_data(replay, states, frame_skip)
@@ -23,7 +31,14 @@ def convert_replays(paths_to_each_replay: List[str], frame_skip: int = 150, verb
     return np.asarray(states)
 
 
-def make_state_data(replay, states, frame_skip):
+def make_state_data(replay: str, states: list, frame_skip: int):
+    """
+    Process single replay
+
+    :param replay: Path to the replay to process
+    :param states: List of all the processed states
+    :param frame_skip: Every frame_skip frame from the replay will be converted
+    """
     replay_iterator = convert_replay(replay)
     for i, value in enumerate(replay_iterator):
         if i % frame_skip == frame_skip - 1:
@@ -33,6 +48,12 @@ def make_state_data(replay, states, frame_skip):
 
 
 def state_to_np_array(game_state: GameState) -> np.ndarray:
+    """
+    Gets only the useful information from game_state and puts it into numpy array
+
+    :param game_state: GameState to convert
+    :return: Numpy array of the most useful information from the game_state
+    """
     whole_state = []
     ball = game_state.ball
     ball_state = np.concatenate((ball.position, ball.linear_velocity, ball.angular_velocity))
