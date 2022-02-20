c676b2b1a3535f59aaa2249034ea52235ab605ea
Matej <matej.novosad.mn@gmail.com>
2022-02-20 08:48:41 +0100

    added the GPU/CPU thingy

--- training_keepaway_bot.py
+++ training_keepaway_bot.py
@@ -1,8 +1,11 @@
+import os
+
 import numpy as np
 import torch.optim
 from rlgym.envs import Match
 from rlgym.utils.action_parsers import DiscreteAction, DefaultAction
-from stable_baselines3 import PPO
+from new_ppo.new_ppo import PPO
+#from stable_baselines3.ppo import PPO
 from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3.common.vec_env import VecMonitor, VecNormalize, VecCheckNan
 from stable_baselines3.ppo import MlpPolicy
@@ -10,20 +13,21 @@ from stable_baselines3.ppo import MlpPolicy
 from rlgym.utils.obs_builders import AdvancedObs
 from rlgym.utils.reward_functions.common_rewards import VelocityBallToGoalReward, EventReward, VelocityPlayerToBallReward
 from rlgym.utils.reward_functions.combined_reward import CombinedReward
-from custom_rewards.keepaway_rewards import KeepAwayReward
+from custom_rewards.keepaway_rewards import AntiJumpReward
 from rlgym.utils.state_setters import DefaultState, RandomState
 from rlgym.utils.terminal_conditions.common_conditions import TimeoutCondition, GoalScoredCondition, NoTouchTimeoutCondition
 from rlgym_tools.sb3_utils import SB3MultipleInstanceEnv
 from rlgym_tools.extra_action_parsers.kbm_act import KBMAction
 from custom_state_setters.proximity_random_setter import ProximityRandomState
+from rlgym_tools.sb3_utils.sb3_instantaneous_fps_callback import SB3InstantaneousFPSCallback
 from custom_state_setters.symmetric_setter import KickoffLikeSetter
-from custom_state_setters.replay_based_setter import ReplayBasedSetter
 from rlgym_tools.extra_state_setters.weighted_sample_setter import WeightedSampleSetter
+from rlgym_tools.extra_state_setters.replay_setter import ReplaySetter
 
 if __name__ == '__main__':  # Required for multiprocessing
     frame_skip = 8  # Number of ticks to repeat an action
     half_life_seconds = 10  # Easier to conceptualize, after this many seconds the reward discount is 0.5
-    n_instances = 6
+    n_instances = 4
     team_size = 1
 
     fps = 120 / frame_skip
@@ -33,24 +37,24 @@ if __name__ == '__main__':  # Required for multiprocessing
     print(f"fps={fps}, gamma={gamma})")
 
     reward_func = CombinedReward(
-        (VelocityBallToGoalReward(), EventReward(goal=10, concede=-10), VelocityPlayerToBallReward()),
-        (0.05, 1, 0.01))
+        (VelocityBallToGoalReward(), VelocityPlayerToBallReward()),
+        (0.05, 0.01))
 
-
+    replays1v1_path = "custom_state_setters/ssl_1v1.npy"
     def get_match():  # Need to use a function so that each instance can call it and produce their own objects
         return Match(
             team_size=team_size,  # 3v3 to get as many agents going as possible, will make results more noisy
             tick_skip=frame_skip,
             reward_function=reward_func,  # Simple reward since example code
             self_play=True,
-            terminal_conditions=[TimeoutCondition(round(fps * 60)),NoTouchTimeoutCondition(round(fps * 15)), GoalScoredCondition()],  # Some basic terminals
+            terminal_conditions=[TimeoutCondition(round(fps * 60)),NoTouchTimeoutCondition(round(fps * 15))],  # Some basic terminals
             obs_builder=AdvancedObs(),  # Not that advanced, good default
-            state_setter=WeightedSampleSetter((ReplayBasedSetter(r'C:\Users\mrkva\PycharmProjects\rlgym_sb3\ssl_replays_for_setter\batched_gamestates'),ProximityRandomState(1000),KickoffLikeSetter(ball_on_ground=False)),(1,1,1)),
+            state_setter=WeightedSampleSetter((KickoffLikeSetter(ball_on_ground=False),RandomState(cars_on_ground=False),ReplaySetter(replays1v1_path)),(10,40,40)),
             action_parser=KBMAction(),  # Discrete > Continuous don't @ me
         )
 
     load_model = input("[L]oad/[T]rain").lower() == "l"
-    env = SB3MultipleInstanceEnv(get_match, n_instances, force_paging=True)  # Start 2 instances, waiting 60 seconds between each
+    env = SB3MultipleInstanceEnv(get_match, n_instances, force_paging=False)  # Start 2 instances, waiting 60 seconds between each
     env = VecCheckNan(env)  # Optional
     env = VecMonitor(env)  # Recommended, logs mean reward and ep_len to Tensorboard
     env = VecNormalize(env, norm_obs=False, gamma=gamma)  # Highly recommended, normalizes rewards
@@ -61,7 +65,7 @@ if __name__ == '__main__':  # Required for multiprocessing
         env,
         n_epochs=32,  # PPO calls for multiple epochs
         learning_rate=1e-4,  # Around this is fairly common for PPO
-        ent_coef=0.01,  # From PPO Atari
+        ent_coef=0.005,  # From PPO Atari
         vf_coef=1.,  # From PPO Atari
         gamma=gamma,  # Gamma as calculated using half-life
         verbose=3,  # Print out all the info as we're going
@@ -71,10 +75,19 @@ if __name__ == '__main__':  # Required for multiprocessing
         device="auto",  # Uses GPU if available
         policy_kwargs={'net_arch': [dict(pi=[256,256,256,256], vf=[256,256,256,256])], 'optimizer_class':torch.optim.Adam}
     )
-    callback = CheckpointCallback(round(5_000_000 / env.num_envs), save_path="policy", name_prefix="rl_model")
+    callback = CheckpointCallback(round(5_000_000 / env.num_envs), save_path="experiment_policy", name_prefix="rl_model")
     if load_model:
-        model = PPO.load("policy/rl_model_80000000_steps.zip", env, custom_objects=dict(n_envs=n_instances*2*team_size))
+        model = PPO.load("experiment_policy/rl_model_error_checkpoint.zip", env, custom_objects=dict(n_envs=n_instances*2*team_size),n_epochs=32)
         env.reset()
-        model.learn(100_000_000_000, callback=callback, reset_num_timesteps=False)
+        try:
+            model.learn(100_000_000_000, callback=callback, reset_num_timesteps=False)
+        except:
+            model.save("experiment_policy/rl_model_error_checkpoint")
+            print("saved on error and save yo arse")
     else:
-        model.learn(100_000_000_000, callback=callback)
+        try:
+            model.learn(100_000_000_000, callback=callback)
+        except:
+            model.save("experiment_policy/rl_model_error_checkpoint")
+            print("saved on error and save yo arse")
+
